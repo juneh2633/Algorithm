cd419e3ee5272a41a21abe6edb63c50549462240
juneh2633 <132862288+juneh2633@users.noreply.github.com>
2023-12-04 00:24:37 +0900

    Create cpp.cpp

---
+++ 백준/cpp.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+typedef pair <int, int> pii;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("./input.txt", "r", stdin);
+    freopen("./output.txt", "w", stdout);
+#endif
+
+    return 0;
+}
