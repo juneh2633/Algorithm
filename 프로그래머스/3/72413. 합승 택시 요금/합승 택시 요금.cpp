f37af7e4823e03f52dc8468b395acac0483c9042
juneh2633 <132862288+juneh2633@users.noreply.github.com>
2024-04-20 00:46:33 +0900

    [level 3] Title: 합승 택시 요금, Time: 0.08 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -6,104 +6,45 @@ typedef pair<ll, ll> pll;
 typedef vector<pii> vii;
 const int INF = 10000001;
 
-
 vector<pii> graph[333];
-int distA[333];
-int distB[333];
-int distC[333];
+vector<vector<int>> dist(3, vector<int>(333, INF)); // 0 for A, 1 for B, 2 for C
 
-void dijA(int start){
-    fill(distA,distA+333,INF);
-    priority_queue<pii> pq;
+void dijkstra(int start, int idx){
+    fill(dist[idx].begin(), dist[idx].end(), INF);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
     pq.push({0, start});
-    distA[start] = 0;
+    dist[idx][start] = 0;
     
     while (!pq.empty()) {
-        int d, v;
-        tie(d, v) = pq.top();
-        d = d * (-1);
+        int d = pq.top().first;
+        int v = pq.top().second;
         pq.pop();
-        if (distA[v] != d) {
-            continue;
-        }
 
-        for (auto &i : graph[v]) {
-            int w, target;
-            tie(target, w) = i;
-            if (distA[target] > distA[v] + w) {
-                distA[target] = distA[v] + w;
-                pq.push({-distA[target], target});
+        if (dist[idx][v] < d) continue;
+
+        for (auto &edge : graph[v]) {
+            int target = edge.first;
+            int w = edge.second;
+            if (dist[idx][target] > dist[idx][v] + w) {
+                dist[idx][target] = dist[idx][v] + w;
+                pq.push({dist[idx][target], target});
             }
         }
-
     }
 }
 
-void dijB(int start){
-    fill(distB,distB+333,INF);
-    priority_queue<pii> pq;
-    pq.push({0, start});
-    distB[start] = 0;
-    
-    while (!pq.empty()) {
-        int d, v;
-        tie(d, v) = pq.top();
-        d = d * (-1);
-        pq.pop();
-        if (distB[v] != d) {
-            continue;
-        }
-
-        for (auto &i : graph[v]) {
-            int w, target;
-            tie(target, w) = i;
-            if (distB[target] > distB[v] + w) {
-                distB[target] = distB[v] + w;
-                pq.push({-distB[target], target});
-            }
-        }
-
-    }
-}
-void dijC(int start){
-    fill(distC,distC+333,INF);
-
-    priority_queue<pii> pq;
-    pq.push({0, start});
-    distC[start] = 0;
-    
-    while (!pq.empty()) {
-        int d, v;
-        tie(d, v) = pq.top();
-        d = d * (-1);
-        pq.pop();
-        if (distC[v] != d) {
-            continue;
-        }
-
-        for (auto &i : graph[v]) {
-            int w, target;
-            tie(target, w) = i;
-            if (distC[target] > distC[v] + w) {
-                distC[target] = distC[v] + w;
-                pq.push({-distC[target], target});
-            }
-        }
-
-    }
-}
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     int answer = INF;
 
-    for(auto &v:fares){
-        graph[v[0]].push_back({v[1],v[2]});
-        graph[v[1]].push_back({v[0],v[2]});
+    for(auto &v : fares){
+        graph[v[0]].push_back({v[1], v[2]});
+        graph[v[1]].push_back({v[0], v[2]});
     }
-    dijA(a);
-    dijB(b);
-    dijC(s);
-    for(int i=1; i<=n;i++){
-        answer=min(answer,distA[i]+distB[i]+distC[i]);
+    dijkstra(a, 0);
+    dijkstra(b, 1);
+    dijkstra(s, 2);
+    for(int i = 1; i <= n; i++){
+        answer = min(answer, dist[0][i] + dist[1][i] + dist[2][i]);
     }
     return answer;
 }
