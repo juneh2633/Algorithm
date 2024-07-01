75df05ad752138f7b4bbc36ecfc7d2ca34a93496
juneh2633 <juneh2633@gmail.com>
2024-07-01 13:44:06 +0900

    [Silver II] Title: 실버와 소수는 둘다 S로 시작한다, Time: 132 ms, Memory: 12216 KB -BaekjoonHub

---
+++ 백준/Silver/29728. 실버와 소수는 둘다 S로 시작한다/실버와 소수는 둘다 S로 시작한다.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int N = 5000000;
+bool isNotPrime[N];
+
+void primeCheck(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        for (int j = i; j <= n; j += i) {
+            if (j == i) {
+                continue;
+            }
+            isNotPrime[j] = 1;
+        }
+    }
+    isNotPrime[2] = 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    memset(isNotPrime, 0, sizeof(isNotPrime));
+    int n;
+    cin >> n;
+    deque<bool> dq;
+    bool rev = 0;
+    primeCheck(n);
+    // b ==1 , s==0
+    dq.push_back(1);
+    for (int i = 2; i <= n; i++) {
+        if (isNotPrime[i]) {
+            if (!rev) {
+                dq.push_back(1);
+                continue;
+            }
+            dq.push_front(1);
+            continue;
+        }
+        if (!rev) {
+            if (dq.back()) {
+                dq.pop_back();
+                dq.push_back(0);
+                dq.push_back(0);
+            } else {
+                dq.push_back(0);
+                rev = 1;
+            }
+        } else {
+            if (dq.front()) {
+                dq.pop_front();
+                dq.push_front(0);
+                dq.push_front(0);
+            } else {
+                dq.push_front(0);
+                rev = 0;
+            }
+        }
+    }
+    int ansB = 0;
+    for (auto &i : dq) {
+        if (i) {
+            ansB++;
+        }
+    }
+    cout << ansB << " " << n - ansB;
+}
